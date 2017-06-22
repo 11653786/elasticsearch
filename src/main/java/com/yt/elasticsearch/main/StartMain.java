2a5590bc7dce990cef521d37fa977f478e147e39
yangtao <yangtao123>
2017-06-22 13:38:51 +0800

    '组合商品接口渠道添加'

---
+++ src/main/java/com/yt/elasticsearch/main/StartMain.java
@@ -0,0 +1,29 @@
+package com.yt.elasticsearch.main;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by PUZE84 on 2017/6/21.
+ */
+public class StartMain {
+    public static void main(String[] args) {
+        // on startup
+
+        TransportClient client = null;
+        try {
+            client = new PreBuiltTransportClient(Settings.EMPTY)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9200));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+
+        client.close();
+    }
+}
